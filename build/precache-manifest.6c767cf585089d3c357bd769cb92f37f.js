ccbc4bad846393a83ed338c48962553900d03ed0
Olga <olga.myazina@outlook.com>
2019-08-02 12:32:33 +0300

    fix

---
+++ build/precache-manifest.6c767cf585089d3c357bd769cb92f37f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9615341a3e216447add6491cd37e1786",
+    "url": "/test-react/build/index.html"
+  },
+  {
+    "revision": "9e7a5b4256c97109c5d3",
+    "url": "/test-react/build/static/css/main.27640681.chunk.css"
+  },
+  {
+    "revision": "bbbabe6db9ffaa904db4",
+    "url": "/test-react/build/static/js/2.66ba51e3.chunk.js"
+  },
+  {
+    "revision": "9e7a5b4256c97109c5d3",
+    "url": "/test-react/build/static/js/main.6e87f0d3.chunk.js"
+  },
+  {
+    "revision": "7389f80ed466af08fd11",
+    "url": "/test-react/build/static/js/runtime~main.114e3aeb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/test-react/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/test-react/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/test-react/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/test-react/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/test-react/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "98300308beffe1eac6cd27673f556928",
+    "url": "/test-react/build/static/media/new-deals__product_1.98300308.jpg"
+  },
+  {
+    "revision": "7cb85f9610309ba9a830bd78a113b0b5",
+    "url": "/test-react/build/static/media/new-deals__product_2.7cb85f96.jpg"
+  },
+  {
+    "revision": "929e12c36e07f6b35cfebff1797f9fe9",
+    "url": "/test-react/build/static/media/new-deals__product_active.929e12c3.jpg"
+  },
+  {
+    "revision": "7477fd1e8831e5b48f43ecdb8a611d3b",
+    "url": "/test-react/build/static/media/product-card__favourite-product-slider-item-1.7477fd1e.png"
+  },
+  {
+    "revision": "3dc1bb2d39dba0437d5960dd26d00fb1",
+    "url": "/test-react/build/static/media/product-card__favourite-product-slider-item-2.3dc1bb2d.png"
+  },
+  {
+    "revision": "2a1868ad3c3b72c5df8a53ee099da5dc",
+    "url": "/test-react/build/static/media/product-card__favourite-product-slider-item-3.2a1868ad.png"
+  },
+  {
+    "revision": "f423f579d1dd97c25a0c6585c906133b",
+    "url": "/test-react/build/static/media/product-catalogue__overlooked-slider-item-1.f423f579.png"
+  },
+  {
+    "revision": "91cb383f3f1fb775254bf2e9d96f7f5e",
+    "url": "/test-react/build/static/media/product-catalogue__overlooked-slider-item-2.91cb383f.png"
+  },
+  {
+    "revision": "9f375324f5dec4499d102cca7adbe753",
+    "url": "/test-react/build/static/media/product-catalogue__overlooked-slider-item-3.9f375324.png"
+  },
+  {
+    "revision": "5e842c7301a612fb2632fdbcbcb5dee2",
+    "url": "/test-react/build/static/media/product-catalogue__overlooked-slider-item-4.5e842c73.png"
+  },
+  {
+    "revision": "350013cd87b92b71a4e7adb49ee6bd09",
+    "url": "/test-react/build/static/media/product-catalogue__overlooked-slider-item-5.350013cd.png"
+  },
+  {
+    "revision": "5e217ba5253e6079c243bef0b195aabd",
+    "url": "/test-react/build/static/media/sales-and-news__item_1.5e217ba5.jpg"
+  },
+  {
+    "revision": "57c5128c1752d71448ee3223b456e52e",
+    "url": "/test-react/build/static/media/sales-and-news__item_2.57c5128c.jpg"
+  },
+  {
+    "revision": "f53060a0f4f10f211dd12521c15b9fd4",
+    "url": "/test-react/build/static/media/sales-and-news__item_3.f53060a0.jpg"
+  },
+  {
+    "revision": "b11b7687a55b887bdd2ea3ea1eaa1687",
+    "url": "/test-react/build/static/media/sales-and-news__item_4.b11b7687.jpg"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/test-react/build/static/media/slider.d613d452.jpg"
+  },
+  {
+    "revision": "54638011c75501dec7517369019fc591",
+    "url": "/test-react/build/static/media/slider180deg.54638011.jpeg"
+  }
+]);
