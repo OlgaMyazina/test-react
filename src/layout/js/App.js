4f566a2558b9a075f8ae567b951aef832abe2241
Olga <olga.myazina@outlook.com>
2019-08-02 12:39:21 +0300

    fix

--- src/layout/js/App.js
+++ src/layout/js/App.js
@@ -19,18 +19,18 @@ const App = () => {
       <HeaderComponent/>
 
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}/>
+        <Route exact path="/" component={Home}/>
         {/*<Route path='/cart' component={Cart}/>*/}
-        <Route path={process.env.PUBLIC_URL + '/product/:id?'} render={(props) => (
+        <Route path="/product/:id?" render={(props) => (
           <Product {...props}/>
         )}/>
-        <Route path={process.env.PUBLIC_URL + '/catalog/:id?'} render={(props) => (
+        <Route path="/catalog/:id?" render={(props) => (
           <Catalog {...props} />
         )}/>
-        <Route path={process.env.PUBLIC_URL + '/productlist'} component={ProductList}/>
-        <Route path={process.env.PUBLIC_URL + '/order'} component={Order}/>
-        <Route path={process.env.PUBLIC_URL + '/favorite'} component={Favorite}/>
-        <Route path={process.env.PUBLIC_URL + '/about'} component={About}/>
+        <Route path="/productlist" component={ProductList}/>
+        <Route path="/order" component={Order}/>
+        <Route path="/favorite" component={Favorite}/>
+        <Route path="/about" component={About}/>
 
       </Switch>
       <Footer/>
